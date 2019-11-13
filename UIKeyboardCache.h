706d7f3bad164d33e8d8b938ee0eefdd1f489c52
ryannair05 <ryannair05@gmail.com>
2019-11-12 20:37:49 -0500

    Clean up Headers

--- UIKeyboardCache.h
+++ UIKeyboardCache.h
@@ -11,25 +11,8 @@
 
 // @property (nonatomic, retain) _UIActionWhenIdle *idleAction;
 
-+ (bool)enabled;
 + (UIKeyboardCache *)sharedInstance;
 
-- (void)_didIdle;
-- (void)_didIdleAndShouldWait;
-- (CGImage *)cachedCompositeImageForCacheKeys:(id)arg1 fromLayout:(id)arg2 opacities:(id)arg3;
-- (CGImage *)cachedImageForKey:(id)arg1 fromLayout:(id)arg2;
-- (void)clearNonPersistentCache;
-- (void)commitTransaction;
-- (void)dealloc;
-- (void)decrementExpectedRender:(id)arg1;
-- (id)displayImagesForView:(id)arg1 fromLayout:(id)arg2 imageFlags:(id)arg3;
-- (void)drawCachedImage:(id)arg1 alpha:(double)arg2 inContext:(CGContext *)arg3;
-- (id)idleAction;
-- (void)incrementExpectedRender:(id)arg1;
-- (id)init;
 - (void)purge;
-- (void)setIdleAction:(id)arg1;
-- (id)uniqueLayoutsFromInputModes:(id)arg1;
-- (void)updateCacheForInputModes:(id)arg1;
 
 @end
